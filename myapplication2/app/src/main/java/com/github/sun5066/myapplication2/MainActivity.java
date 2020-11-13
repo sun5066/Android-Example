00b1744a5a5db3f141ed9a66b65d68c6d2816c94
sun5066 <alstjr4434@naver.com>
2020-11-13 15:29:46 +0900

    origin

---
+++ myapplication2/app/src/main/java/com/github/sun5066/myapplication2/MainActivity.java
@@ -0,0 +1,19 @@
+package com.github.sun5066.myapplication2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
