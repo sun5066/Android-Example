3ae02c432dee49f017ce62cf16721d92a231ef2c
lovelyhyeony <dmlgus1222@naver.com>
2020-11-15 02:32:56 +0900

    11-15

--- myapplication2/app/src/main/java/com/github/sun5066/myapplication2/MainActivity.java
+++ myapplication2/app/src/main/java/com/github/sun5066/myapplication2/MainActivity.java
@@ -1,5 +1,8 @@
 package com.github.sun5066.myapplication2;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,12 +11,29 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+        LinearLayout linearLayout = new LinearLayout(this);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+
+        Button button1 = new Button(this);
+        button1.setText("버튼1");
+        button1.setLayoutParams(params);
+        linearLayout.addView(button1);
+
+        setContentView(R.layout.activity_main);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+    }
+
+    public void onButton1Clicked(View view) {
+        System.out.println("버튼1 클릳됨");
     }
 }
